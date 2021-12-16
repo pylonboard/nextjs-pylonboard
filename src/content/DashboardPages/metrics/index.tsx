b75fbad501138b4feacd35fdfd37547fc61cc3ab
Dionisis <dio@skarp.dk>
2021-12-16 14:22:19 +0200

    Implements metrics page

---
+++ src/content/DashboardPages/metrics/index.tsx
@@ -0,0 +1,73 @@
+import { Grid } from '@mui/material';
+
+import PageTitleWrapper from 'src/components/PageTitleWrapper';
+
+import PageHeader from '@/components/PageHeader';
+import { gql, useQuery } from '@apollo/client';
+import Loader from '@/components/Loader';
+import MetricsTable from '@/content/Dashboards/Metrics/MetricsTable';
+
+const QUERY = gql`
+    query MineRankings {
+        mineRankings {
+            percentile99 {
+                percentileFloor
+                walletsIncluded
+                amountOfMine
+            }
+            percentile95 {
+                percentileFloor
+                walletsIncluded
+                amountOfMine
+            }
+            percentile90 {
+                percentileFloor
+                walletsIncluded
+                amountOfMine
+            }
+            percentile75 {
+                percentileFloor
+                walletsIncluded
+                amountOfMine
+            }
+            floor {
+                percentileFloor
+                walletsIncluded
+                amountOfMine
+            }
+        }
+    }
+`;
+
+function DashboardMetricsContent() {
+  const { data, loading } = useQuery(QUERY);
+
+  if (loading) {
+    return <Loader />
+  }
+
+  return (
+    <>
+      <PageTitleWrapper>
+        <PageHeader
+          title="Pylon Mine Metrics"
+          subtitle="These are the Pylon Mine Percentile Rankings"
+        />
+      </PageTitleWrapper>
+      <Grid
+        sx={{ px: 4 }}
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="stretch"
+        spacing={3}
+      >
+        <Grid item xs={12}>
+          <MetricsTable data={data.mineRankings} />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default DashboardMetricsContent;
