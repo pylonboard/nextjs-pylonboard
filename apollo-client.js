b6c19eb56dc0568d3ec1da1f9ef001b3163c8ec8
Dionisis <dio@skarp.dk>
2021-12-16 14:19:45 +0200

    Removes typeName prop from graphql response

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,10 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://pylonboard-backend-ylc4b.ondigitalocean.app/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    addTypename: false
+  }),
 });
 
 export default client;
