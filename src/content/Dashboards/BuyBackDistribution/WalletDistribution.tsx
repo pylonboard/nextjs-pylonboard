cc954e1a09c8335b71890ecfecd250cdf3b297c3
Dionisis <dio@skarp.dk>
2021-12-23 00:12:53 +0200

    Fix identation

--- src/content/Dashboards/BuyBackDistribution/WalletDistribution.tsx
+++ src/content/Dashboards/BuyBackDistribution/WalletDistribution.tsx
@@ -161,38 +161,38 @@ function WalletDistribution() {
       <Divider />
 
       {isLoading ? (
-          <Box px={3} pb={3}>
-            <TableContainer>
-              <Table>
-                <TableHeadWrapper>
-                  <TableRow>
-                    <TableCell>{'Tx Hash'}</TableCell>
-                    <TableCell>{'UST'}</TableCell>
-                    <TableCell align="center">{'MINE'}</TableCell>
-                    <TableCell align="right">{'Date'}</TableCell>
+        <Box px={3} pb={3}>
+          <TableContainer>
+            <Table>
+              <TableHeadWrapper>
+                <TableRow>
+                  <TableCell>{'Tx Hash'}</TableCell>
+                  <TableCell>{'UST'}</TableCell>
+                  <TableCell align="center">{'MINE'}</TableCell>
+                  <TableCell align="right">{'Date'}</TableCell>
+                </TableRow>
+              </TableHeadWrapper>
+              <TableBody>
+                {Array.from(Array(10), Math.random).map(value => (
+                  <TableRow key={value}>
+                    <TableCell>
+                      <Skeleton />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton />
+                    </TableCell>
                   </TableRow>
-                </TableHeadWrapper>
-                <TableBody>
-                  {Array.from(Array(10), Math.random).map(value => (
-                    <TableRow key={value}>
-                      <TableCell>
-                        <Skeleton />
-                      </TableCell>
-                      <TableCell>
-                        <Skeleton />
-                      </TableCell>
-                      <TableCell>
-                        <Skeleton />
-                      </TableCell>
-                      <TableCell>
-                        <Skeleton />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Box>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       ) : (
         <Box px={3} pb={3}>
           {(!!tableData.length) && (
