a8f6603a0498e4827226a936f745308b304326cd
Dionisis <dio@skarp.dk>
2021-12-23 13:45:39 +0200

    Updates app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,12 @@ import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Router from 'next/router';
-import nProgress from 'nprogress';
-import 'nprogress/nprogress.css';
 import ThemeProvider from 'src/theme/ThemeProvider';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import createEmotionCache from 'src/createEmotionCache';
 import { SidebarProvider } from 'src/contexts/SidebarContext';
-import 'src/utils/chart';
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from 'src/store';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import useScrollTop from 'src/hooks/useScrollTop';
-import { SnackbarProvider } from 'notistack';
 import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
 import { NewRelicSnippet } from '@/components/NewRelicSnippet';
@@ -38,16 +29,12 @@ const isDev = process.env.NODE_ENV === 'development'
 function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   const getLayout = Component.getLayout ?? ((page) => page);
-  useScrollTop();
-
-  Router.events.on('routeChangeStart', nProgress.start);
-  Router.events.on('routeChangeError', nProgress.done);
-  Router.events.on('routeChangeComplete', nProgress.done);
+  useScrollTop()
 
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>Tokyo Black NextJS Typescript Admin Dashboard</title>
+        <title>Pylon Board</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -56,26 +43,14 @@ function MyApp(props: MyAppProps) {
           <NewRelicSnippet />
         )}
       </Head>
-      <ReduxProvider store={store}>
-        <SidebarProvider>
-          <ThemeProvider>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <SnackbarProvider
-                maxSnack={6}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right'
-                }}
-              >
-                <CssBaseline />
-                <ApolloProvider client={client}>
-                  {getLayout(<Component {...pageProps} />)}
-                </ApolloProvider>
-              </SnackbarProvider>
-            </LocalizationProvider>
-          </ThemeProvider>
-        </SidebarProvider>
-      </ReduxProvider>
+      <SidebarProvider>
+        <ThemeProvider>
+          <CssBaseline />
+          <ApolloProvider client={client}>
+            {getLayout(<Component {...pageProps} />)}
+          </ApolloProvider>
+        </ThemeProvider>
+      </SidebarProvider>
     </CacheProvider>
   );
 }
